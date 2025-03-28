a66346d7d98245cb9884c21eb577131500571b58
Robbe Decroo <r0840260@student.thomasmore.be>
2025-03-29 00:46:55 +0100

    fixed colors in timeline

--- src/app/components/timeline/timeline-item.tsx
+++ src/app/components/timeline/timeline-item.tsx
@@ -14,12 +14,12 @@ export function TimelineItem({month, finalMonth, completed, isSelected, isPrevio
 
     return (
         <li>
-            { month != 'Januari' ? !isPreviousCompleted ? month === finalMonth ? <hr /> : <hr className='bg-red-500' /> : <hr className="bg-green-500" /> : null }
+            { month != 'Januari' ? !isPreviousCompleted ? <hr className='bg-red-500' /> : <hr className="bg-green-500" /> : null }
             <div className="timeline-middle">
                 <TimelineIcon completed={completed} current={month == finalMonth} />
             </div>
             <div className={`timeline-end timeline-box ms-4 text-left cursor-pointer hover:shadow-md min-w-24 ${isSelected ? 'shadow-md ms-6' : null}`} onClick={() => setSelectedMonth(month)}>{month}</div>
-            { month != finalMonth ? !completed ? isNextFinal ? <hr /> : <hr className='bg-red-500' /> : <hr className='bg-green-500'/> : null }
+            { month != finalMonth ? !completed ? <hr className='bg-red-500' /> : <hr className='bg-green-500'/> : null }
         </li>
     );
 }
